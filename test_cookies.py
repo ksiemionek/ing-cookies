b48d8aaa8fba55af2796303f6d30a172237bb88b
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-04-20 00:42:06 +0200

    remove redundant assertion

--- test_cookies.py
+++ test_cookies.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page, expect
 
+
 def test_cookies(page: Page):
     page.goto("https://www.ing.pl")
     
@@ -12,11 +13,10 @@ def test_cookies(page: Page):
     
     cookies = page.context.cookies()
     cookie_names = [c['name'] for c in cookies]
-    
+
     assert 'cookiePolicyGDPR' in cookie_names, "cookiePolicyGDPR not found"
     assert 'cookiePolicyGDPR__details' in cookie_names, "cookiePolicyGDPR__details not found"
 
     cookie_gdpr = next((c for c in cookies if c['name'] == 'cookiePolicyGDPR'), None)
 
-    assert cookie_gdpr is not None, "cookiePolicyGDPR not found"
     assert cookie_gdpr['value'] == '3', "cookiePolicyGDPR value is not 3"
